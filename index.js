6011b351dbc2837ab9f57c9b34a623939093ecae
Nimer Bashir <nibashir1@gmail.com>
2018-06-06 19:07:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,8 @@ function sayHiToGrandma(string) {
     return "I can't hear you!"
   }
 }
+function sayHiToGrandma(string) {
+  if(string==="I love you, Grandma.") {
+    return "I love you, too"
+  }
+}
