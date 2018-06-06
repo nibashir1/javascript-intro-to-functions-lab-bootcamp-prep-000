fbd9e0d9108041f06ced4a14d7a1e5edf49f174b
Nimer Bashir <nibashir1@gmail.com>
2018-06-06 19:17:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,10 +13,6 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if(string = string.toLowerCase) {
     return "I can't hear you!"
+  }else if{
+  return "YES INDEED!"  
   }
-}
-function sayHiToGrandma(string) {
-  if(string == "I love you, Grandma.") {
-    return "I love you, too."
-  }
-}
